33c21ae09301d00e458f2fd6d02ec445c2e8c0f6
LBoll <lboll582@gmail.com>
2017-11-26 21:23:00 -0500

    first commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ListCustomers()
+        {
+            // 1. Creating an object for the ORM 
+            CoffeeAppDBEnt ORM = new CoffeeAppDBEnt();
+
+            // 2. Load the data from the DbSet into a data structure 
+            List<Product> CustomerList = ORM.Products.ToList();
+
+            // 3. Filter the data (Optional)
+
+            ViewBag.CustomerList = CustomerList;
+
+            //ViewBag.CountryList = ORM.Customers.Select(x => x.Country).Distinct().ToList();
+
+            return View("ProductView");
+        }
+
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUser(User NewUserRecord)
+        {
+            // 1. validation 
+
+            if (ModelState.IsValid)
+            {
+                // 2. add the new record to the ORM, save changes to db
+                CoffeeAppDBEnt ORM = new CoffeeAppDBEnt();
+
+                ORM.Users.Add(NewUserRecord);
+                ORM.SaveChanges();
+
+                // 3. showing the list of all customers
+                return RedirectToAction("ListCustomers");
+
+            }
+            else
+            {    // If validation fails 
+                // go back to the form and show the list of errors!
+                return View("AddUser");
+            }
+        }
+        /*
+        public ActionResult UserLogin(User Login)
+        {
+            CoffeeAppDBEnt ORM = new CoffeeAppDBEnt();
+            if ()
+
+            return RedirectToAction("ListCustomers");
+        }
+        */
+    }
+}
---
+++ CoffeeShop/Models/Register.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.Models
+{
+    public class Register
+    {
+
+        private string firstname;
+        private string lastname;
+        private string email;
+        private string phonenumber;
+        private string password;
+
+        public Register()
+        {
+            firstname = "";
+            lastname = "";
+            email = "";
+            phonenumber = "";
+            password = "";
+
+        }
+
+        public Register(string firstname, string lastname, string email, string phonenumber, string password)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.phonenumber = phonenumber;
+            this.password = password;
+        }
+
+        public string FirstName
+        {
+            set { firstname = value; }
+            get { return firstname; }
+        }
+
+        public string LastName
+        {
+            set { lastname = value; }
+            get { return lastname; }
+        }
+
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+        }
+
+        public string PhoneNumber
+        {
+            set { phonenumber = value; }
+            get { return phonenumber; }
+        }
+
+        public string Password
+        {
+            set { password = value; }
+            get { return password; }
+        }
+    }
+}
